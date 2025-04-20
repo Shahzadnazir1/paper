6a5e418184ec653150ca3d5086dc4b30a643cbe0
Shahzadnazir1 <shahzadnazir2021@gmail.com>
2025-04-20 20:18:10 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+ using System;
+class program
+{
+    static void Main(String[] args)
+    {
+
+        string name = "shahzad";
+        int age = 20;
+        float CGPA = 3.5f;
+        long largenumber = 556698745;
+        Console.WriteLine("Name " + name);
+        Console.WriteLine("Age " + age);
+        Console.WriteLine("CGPA " + CGPA);
+        Console.WriteLine("largenumber " + largenumber);
+       
+
+    }
+}
